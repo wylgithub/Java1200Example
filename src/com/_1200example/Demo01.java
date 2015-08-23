7c0c7a0c26231a2695c2dba3da9fd334b3c5a6c3
wylgithub <ltwuyanlong@sina.com>
2015-08-23 23:27:08 +0800

    开始学习Java基础啊练习：Java学习1200例子

---
+++ src/com/_1200example/Demo01.java
@@ -0,0 +1,15 @@
+package com._1200example;
+
+public class Demo01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//method01使用System.out.println();打印等腰三角形
+		System.out.println("     *     ");
+		System.out.println("    ***    ");
+		System.out.println("   *****   ");
+		System.out.println("  *******  ");
+		System.out.println(" ********* ");
+	}
+
+}
